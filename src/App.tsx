8a9218964faf3d0c5a2cf24fec0ced662fb428e5
Stanley Yao <yclive0925@gmail.com>
2023-02-06 22:50:51 -0700

    build machine

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import AppBar from './components/shared/AppBar';
 import { Api } from './utils/apis';
-import useDeepCompareEffect from 'use-deep-compare-effect'
-import { AvailableDateObject, CheckDatesResponse } from './types';
+import { AvailableDateObject } from './types';
 
 function App() {
 
-  const [isLoading, setIsLoading] = useState(false)
   const [data, setData] = useState<AvailableDateObject[]>([])
 
   useEffect(() => {
     const getDates = async () => {
-      setIsLoading(true)
       const dates = await Api.getDates('Canada', 'USA')
-      console.log("🚀 ~ file: App.tsx:12 ~ getDates ~ dates", dates)
-      setIsLoading(false)
       setData(dates.data.Items)
     }
     getDates()
