cea5fd3be46763ced87bfee72f1389b59413dc85
Stanley Yao <yclive0925@gmail.com>
2023-02-06 20:56:44 -0700

    added a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import AppBar from './components/shared/AppBar';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  return (    
+    <div>
+      <AppBar />
+      <h1 className="text-3xl font-bold underline">
+
+        Hello world!
+      </h1>
+    </div>
   );
 }
 
